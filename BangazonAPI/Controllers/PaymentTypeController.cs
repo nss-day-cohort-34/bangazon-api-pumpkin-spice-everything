b97a44e483031c3a5aebcd5412d16d4c4280b4a3
Carl Barringer <cbarringer1@gmail.com>
2019-11-06 12:28:10 -0600

    added post and get by id

--- BangazonAPI/Controllers/PaymentTypeController.cs
+++ BangazonAPI/Controllers/PaymentTypeController.cs
@@ -32,15 +32,16 @@ namespace BangazonAPI.Controllers
 
         // GET: api/PaymentType
         [HttpGet]
-        public async Task<IActionResult> GetAllCustomers()
+        public async Task<IActionResult> GetAllPaymentTypes()
         {
             using (SqlConnection conn = Connection)
             {
                 conn.Open();
                 using (SqlCommand cmd = conn.CreateCommand())
                 {
-                    cmd.CommandText = @"SELECT Id, AcctNumber, Type, CustomerId
-                                          FROM PaymentType";
+                    cmd.CommandText = @"SELECT p.Id, AcctNumber, p.[Type], p.CustomerId, c.Id, c.FirstName, c.LastName
+                                        FROM PaymentType p 
+                                        LEFT JOIN Customer c ON p.CustomerId = c.Id";
                     SqlDataReader reader = await cmd.ExecuteReaderAsync();
 
                     Dictionary<int, PaymentType> paymenttypes = new Dictionary<int, PaymentType>();
@@ -53,6 +54,12 @@ namespace BangazonAPI.Controllers
                             AcctNumber = reader.GetString(reader.GetOrdinal("AcctNumber")),
                             Type = reader.GetString(reader.GetOrdinal("Type")),
                             CustomerId = reader.GetInt32(reader.GetOrdinal("CustomerId")),
+                            Customer = new Customer()
+                                {
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                LastName = reader.GetString(reader.GetOrdinal("LastName")),
+       
+                                }
                         };
 
                         paymenttypes.Add(paymentTypeId, newPaymentType);
@@ -66,16 +73,70 @@ namespace BangazonAPI.Controllers
         }
 
         // GET: api/PaymentType/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetPaymentTypes([FromRoute] int id)
         {
-            return "value";
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT p.Id, AcctNumber, p.[Type], p.CustomerId, c.Id, c.FirstName, c.LastName
+                                        FROM PaymentType p 
+                                        LEFT JOIN Customer c ON p.CustomerId = c.Id
+                                        WHERE p.Id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+                    PaymentType paymenttype = null;
+                    if (reader.Read())
+                    {
+                        paymenttype = new PaymentType
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            AcctNumber = reader.GetString(reader.GetOrdinal("AcctNumber")),
+                            Type = reader.GetString(reader.GetOrdinal("Type")),
+                            CustomerId = reader.GetInt32(reader.GetOrdinal("CustomerId")),
+                            Customer = new Customer()
+                            {
+                                FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                            }
+                        };
+                    }
+
+                    reader.Close();
+
+                    return Ok(paymenttype);
+                }
+            }
         }
 
         // POST: api/PaymentType
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<IActionResult> Post([FromBody] PaymentType paymentType)
         {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    // More string interpolation
+                    cmd.CommandText = @"
+                        INSERT INTO PaymentType (AcctNumber, [Type], CustomerId)
+                        OUTPUT INSERTED.Id
+                        VALUES (@acctNumber, @type, @customerId)
+                    ";
+                    cmd.Parameters.Add(new SqlParameter("@acctNumber", paymentType.AcctNumber));
+                    cmd.Parameters.Add(new SqlParameter("@type", paymentType.Type));
+                    cmd.Parameters.Add(new SqlParameter("@customerId", paymentType.CustomerId));
+
+                    paymentType.Id = (int)await cmd.ExecuteScalarAsync();
+
+                    //return CreatedAtRoute("GetPaymentType", new { id = paymentType.Id }, paymentType);
+                    return Ok(paymentType);
+                }
+            }
         }
 
         // PUT: api/PaymentType/5
@@ -86,8 +147,25 @@ namespace BangazonAPI.Controllers
 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<IActionResult> Delete([FromRoute] int id)
         {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"DELETE FROM PaymentType 
+                                        WHERE id = @id";
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+                    if (rowsAffected > 0)
+                    {
+                        return new StatusCodeResult(StatusCodes.Status204NoContent);
+                    }
+                    throw new Exception("No rows affected");
+                }
+            }
         }
     }
 }
--- Test.sql
+++ Test.sql
@@ -1,2 +1,18 @@
-﻿select * from orderproduct;
+﻿SELECT p.Id, AcctNumber, p.[Type], p.CustomerId, c.Id, c.FirstName, c.LastName
+FROM PaymentType p 
+LEFT JOIN Customer c ON p.CustomerId = c.Id;
+select * from customer;
 
+;
+--delete from PaymentType where Id = 13;
+
+INSERT INTO PaymentType (AcctNumber, Type, CustomerId)
+                        OUTPUT INSERTED.Id
+                        VALUES (99999, 'testing', 1)
+Select * from customer where customer.id = Id;
+select * from PaymentType;
+
+select * from PaymentType;
+select * from customer;
+
+--delete from customer where id = 10;
