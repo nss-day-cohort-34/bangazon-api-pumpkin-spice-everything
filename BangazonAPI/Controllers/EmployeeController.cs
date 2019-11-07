3a42734f69ad6ea9843724c54c243ec1833777a8
Allison Patton <allison.tpttn@gmail.com>
2019-11-07 13:57:38 -0600

    working on get one employee

--- BangazonAPI/Controllers/EmployeeController.cs
+++ BangazonAPI/Controllers/EmployeeController.cs
@@ -72,6 +72,7 @@ namespace BangazonAPI.Controllers
                                 {
                                     Id = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
                                     Name = reader.GetString(reader.GetOrdinal("DepartmentName")),
+                                    Budget = reader.GetInt32(reader.GetOrdinal("Budget"))
                                 }
                             };
                             employees.Add(employeeId, newEmployee);
@@ -92,7 +93,8 @@ namespace BangazonAPI.Controllers
                                     DecommissionDate = reader.GetDateTime(reader.GetOrdinal("DecomissionDate"))
                                 };
                                 fromDictionary.Computers.Add(newComputer);
-                            } else
+                            }
+                            else
                             {
                                 Computer newComputer = new Computer()
                                 {
@@ -117,11 +119,97 @@ namespace BangazonAPI.Controllers
             }
         }
 
-        // GET: api/Employee/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        // GET: api/Employee/1 ---code for get one employee
+        [HttpGet("{id}", Name = "GetEmployee")]
+        public async Task<IActionResult> GetEmployee(int id)
         {
-            return "value";
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"  
+                                       SELECT e.Id AS EmployeeId, e.FirstName, e.LastName, e.DepartmentId, 
+                                          e.IsSupervisor, e.StartDate, d.Id AS DepartmentId, d.Name AS DepartmentName, d.Budget, 
+                                          c.Id AS ComputerId, c.Manufacturer, c.Make, c.PurchaseDate, c.DecomissionDate, 
+                                          ce.Id AS ComputerEmployeeId
+                                       FROM Employee e
+                                          LEFT JOIN Department d ON e.DepartmentId = d.Id
+                                          LEFT JOIN ComputerEmployee ce ON ce.EmployeeId = e.Id
+                                          WHERE EmployeeId = @id";
+
+                    cmd.Parameters.Add(new SqlParameter("@id", id));
+                    SqlDataReader reader = await cmd.ExecuteReaderAsync();
+
+                    Dictionary<int, ComputerEmployee> computerEmployees = new Dictionary<int, ComputerEmployee>();
+
+                    while (reader.Read())
+                    {
+                        int computerEmployeeId = reader.GetInt32(reader.GetOrdinal("ComputerEmployeeId")); //get the id
+                        if (!computerEmployees.ContainsKey(computerEmployeeId)) //have I seen this employee before?
+                        {
+                            ComputerEmployee newComputerEmployee = null;
+
+                            newComputerEmployee = new ComputerEmployee
+                            {
+                                Id = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
+                                EmployeeId = reader.GetInt32(reader.GetOrdinal("EmployeeId")),
+                                Employee = new Employee()
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("EmployeeId")),
+                                    FirstName = reader.GetString(reader.GetOrdinal("FirstName")),
+                                    LastName = reader.GetString(reader.GetOrdinal("LastName")),
+                                    DepartmentId = reader.GetInt32(reader.GetOrdinal("DepartmentId")),
+                                    IsSupervisor = reader.GetBoolean(reader.GetOrdinal("IsSupervisor")),
+                                    StartDate = reader.GetDateTime(reader.GetOrdinal("StartDate"))
+                                },
+                                ComputerId = reader.GetInt32(reader.GetOrdinal("ComputerId")),
+                                Computer = new Computer()
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("ComputerId")),
+                                    Make = reader.GetString(reader.GetOrdinal("Make")),
+                                    Manufacturer = reader.GetString(reader.GetOrdinal("Manufacturer")),
+                                    PurchaseDate = reader.GetDateTime(reader.GetOrdinal("PurchaseDate"))
+                                },
+                                    AssignDate = reader.GetDateTime(reader.GetOrdinal("AssignDate")),
+                                UnassignDate = reader.GetDateTime(reader.GetOrdinal("UnassignDate")),
+                            };
+                            computerEmployees.Add(computerEmployeeId, newComputerEmployee);
+                        }
+
+                        Employee fromDictionary = employees[employeeId];
+
+                        if (!reader.IsDBNull(reader.GetOrdinal("ComputerId"))) //if i have an computer id then it will run.
+                        {
+                            if (!reader.IsDBNull(reader.GetOrdinal("DecomissionDate")))
+                            {
+                                Computer newComputer = new Computer()
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("ComputerId")),
+                                    Make = reader.GetString(reader.GetOrdinal("Make")),
+                                    Manufacturer = reader.GetString(reader.GetOrdinal("Manufacturer")),
+                                    PurchaseDate = reader.GetDateTime(reader.GetOrdinal("PurchaseDate")),
+                                    DecommissionDate = reader.GetDateTime(reader.GetOrdinal("DecomissionDate"))
+                                };
+                                fromDictionary.Computers.Add(newComputer);
+                            }
+                            else
+                            {
+                                Computer newComputer = new Computer()
+                                {
+                                    Id = reader.GetInt32(reader.GetOrdinal("ComputerId")),
+                                    Make = reader.GetString(reader.GetOrdinal("Make")),
+                                    Manufacturer = reader.GetString(reader.GetOrdinal("Manufacturer")),
+                                    PurchaseDate = reader.GetDateTime(reader.GetOrdinal("PurchaseDate")),
+                                };
+                                fromDictionary.Computers.Add(newComputer);
+                            }
+                        }
+                        reader.Close();
+                        return Ok(employees.Values.First());
+                    }
+                }
+            }
         }
 
         // POST: api/Employee
